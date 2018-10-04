c85b6be0fbdab25ff43721d09539c9375e6fe5a6
Daniel Schoch <d.schoch90@gmail.com>
2018-10-04 02:11:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -84,6 +84,17 @@ class Student
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.slice(0,x)
+  end
 
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.slice(0,x)
   end
 end
